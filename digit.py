f56cfb28e40f8da742df6249d3476a4a44b0981c
Magnus Ericmats <majje@think.(none)>
2012-09-18 22:03:26 +0200

    Clean up comments and imports.

--- digit.py
+++ digit.py
@@ -2,20 +2,16 @@
 """
 Created on Mon Sep 10 21:39:36 2012
 
-@author: majje
+@author: Magnus Ericmats
 """
 
-from scipy import delete
-#from scipy import mat
+from matplotlib.pyplot import imshow
+from matplotlib.cm import get_cmap
+from matplotlib.pyplot import draw, show
 from scipy.io import loadmat
-from numpy import random
-#import matplotlib
-#from pylab import imshow
-#from pylab import get_cmap
-#from pylab import hold
-from pylab import *
-
-#from pylab import *
+from pylab import sqrt, floor, ceil, zeros, divide, remainder, permutation
+from pylab import concatenate
+from numpy.core.fromnumeric import reshape
 
 input_layer_size = 20 * 20
 hidden_layer_size = 25
@@ -35,20 +31,16 @@ def displayData(X):
     print "Display columns:", display_cols
         
     display = zeros((display_rows*height,display_cols*width))
+
+    # Iterate through the training sets, reshape each one and populate
+    # the display matrix with the letter matrixes.    
     for xrow in range(0, m):
-        #print "xrow", xrow
         rowindex = divide(xrow, display_cols)
         columnindex = remainder(xrow, display_cols)
-        #print "rowindex", rowindex
-        #print "columnindex", columnindex
         rowstart = int(rowindex*height)
         rowend = int((rowindex+1)*height)
         colstart = int(columnindex*width)
         colend = int((columnindex+1)*width)
-        #print "row start", rowstart
-        #print "row end", rowend
-        #print "col start", colstart 
-        #print "col end", colend
         display[rowstart:rowend, colstart:colend] = X[xrow,:].reshape(height,width).transpose()
          
     imshow(display, cmap=get_cmap('binary'), interpolation='none')
@@ -74,23 +66,6 @@ def loadMatlabData(filename):
     
     displayData(X[random_columns,:])
     
-    # Number of X/y pairs
-    #mdata = Xdata.shape[0]
-    #print "Number of X/y pairs:", mdata
-    #print "Restructure training data into new test, cross validation and train sets"
-    # get the y data from Xdata
-    
-    #X =     Xdata[                     0:mtrain, :]
-    #Xval =  Xdata[           mtrain:mtrain+mval, :]
-    #Xtest = Xdata[mtrain+mval:mtrain+mval+mtest, :]
-    
-    #y =     ydata[                     0:mtrain]
-    #yval =  ydata[           mtrain:mtrain+mval]
-    #ytest = ydata[mtrain+mval:mtrain+mval+mtest]
-    
-    #print "X train:", X.shape[0], "val:", Xval.shape[0], "test:", Xtest.shape[0]     
-    #print "y train:", y.shape[0], "val:", yval.shape[0], "test:", ytest.shape[0] 
-    #return X, y, Xval, yval, Xtest, ytest
     return X,y
 
 def loadWeights(filename):
